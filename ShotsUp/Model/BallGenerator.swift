6a4b0b38874584825477412ce9d637e04e68b90c
lillytoma <lilyanntoma@icloud.com>
2025-02-12 11:58:00 -0500

    making sure enough balls are created

---
+++ ShotsUp/Model/BallGenerator.swift
@@ -0,0 +1,18 @@
+//
+//  BallGenerator.swift
+//  ShotsUp
+//
+//  Created by Lilly Toma on 2/12/25.
+//
+
+import SwiftUI
+
+struct BallGenerator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BallGenerator()
+}
---
+++ ShotsUp/Model/View/InstructionPage.swift
@@ -0,0 +1,18 @@
+//
+//  InstructionPage.swift
+//  ShotsUp
+//
+//  Created by Lilly Toma on 2/10/25.
+//
+
+import SwiftUI
+
+struct InstructionPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InstructionPage()
+}
